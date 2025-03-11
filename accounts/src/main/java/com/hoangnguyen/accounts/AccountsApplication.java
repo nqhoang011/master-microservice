42d84a4e1fd7284060499dd6bbfffb5403e1ba11
Hoang Nguyen <133132462+nqhoang011@users.noreply.github.com>
2025-03-11 15:41:35 +0700

    upload src

---
+++ accounts/src/main/java/com/hoangnguyen/accounts/AccountsApplication.java
@@ -0,0 +1,40 @@
+package com.hoangnguyen.accounts;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Account microservice REST API Documentation",
+				description = "Bank accounts microservice REST API documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Hoang Nguyen",
+						email = "hoangnq011@gmail.com",
+						url = ""
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = ""
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Bank accounts microservice REST API documentation",
+				url = "http://localhost:8080/swagger-ui/index.html#/"
+		)
+)
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+}
