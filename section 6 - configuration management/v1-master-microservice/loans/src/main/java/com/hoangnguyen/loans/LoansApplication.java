6e73613921535b1c00acefac4d7019a30e5d95b4
nqhoang011 <hoangnq011@gmail.com>
2025-04-13 15:28:35 +0700

    feat: add section 6

---
+++ section 6 - configuration management/v1-master-microservice/loans/src/main/java/com/hoangnguyen/loans/LoansApplication.java
@@ -0,0 +1,45 @@
+package com.hoangnguyen.loans;
+
+import com.hoangnguyen.loans.dto.LoansContactInfoDto;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.hoangnguyen.loans.controller") })
+@EnableJpaRepositories("com.hoangnguyen.loans.repository")
+@EntityScan("com.hoangnguyen.loans.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {LoansContactInfoDto.class})
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Loans microservice REST API Documentation",
+				description = "Bank Loans microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Hoang Nguyen",
+						email = "hoangnq011@gmail.com",
+						url = ""
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = ""
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Bank Loans microservice REST API Documentation",
+				url = "https://localhost:8090/swagger-ui.html"
+		)
+)
+public class LoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansApplication.class, args);
+	}
+}
