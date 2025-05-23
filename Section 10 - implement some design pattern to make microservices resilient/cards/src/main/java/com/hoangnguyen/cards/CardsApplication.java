b9c97a1c6a29e77895f71da9c2c0511702437f4e
nqhoang011 <hoangnq011@gmail.com>
2025-05-23 21:52:11 +0700

    add section 10 - implement patterns to make microservices resilient

---
+++ Section 10 - implement some design pattern to make microservices resilient/cards/src/main/java/com/hoangnguyen/cards/CardsApplication.java
@@ -0,0 +1,46 @@
+package com.hoangnguyen.cards;
+
+import com.hoangnguyen.cards.dto.CardsContactInfoDto;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.hoangnguyen.cards.controller") })
+@EnableJpaRepositories("com.hoangnguyen.cards.repository")
+@EntityScan("com.hoangnguyen.cards.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {CardsContactInfoDto.class})
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cards microservice REST API Documentation",
+				description = "Bank Cards microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Hoang Nguyen",
+						email = "hoangnq011@gmail.com",
+						url = ""
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = ""
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Bank Cards microservice REST API Documentation",
+				url = "https://localhost:9000/swagger-ui.html"
+		)
+)
+public class CardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsApplication.class, args);
+	}
+
+}
